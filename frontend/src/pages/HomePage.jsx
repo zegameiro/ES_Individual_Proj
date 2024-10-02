535f43b7565cb44e6f4fe5d62712d862dffc95e3
zegameiro <jose.mcgameiro@ua.pt>
2024-10-02 22:31:17 +0100

    Created navbar component and homepage

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,28 @@
+import NavBarComp from "../components/NavBarComp"
+import { GoogleLogin } from "@react-oauth/google"
+
+const HomePage = () => {
+
+    const responseMessage = (response) => {
+        console.log("Response received from Google:", response)
+    }
+
+    const errorMessage = (error) => {
+        console.error("Error response from Google:", error)
+    }
+
+    return (
+        <div className="flex flex-col">
+            <NavBarComp />
+            <div className="px-[10vw] py-[4vh]">
+                <h1 className="text-2xl font-bold pb-10">Homepage</h1>
+                <GoogleLogin
+                    onSuccess={responseMessage}
+                    onError={errorMessage}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
