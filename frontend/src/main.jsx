f7023e74cbf88bd7e95ce467ede068d967a35d4e
zegameiro <jose.mcgameiro@ua.pt>
2024-10-02 22:31:02 +0100

    Added Google oauth provider and darkmode from nextui to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { NextUIProvider } from '@nextui-org/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
 import './index.css'
+import { clientId, clientSecret } from './constants.jsx'
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <NextUIProvider>
-        <App />
-      </NextUIProvider>
-    </QueryClientProvider>
-  </StrictMode>,
+  <GoogleOAuthProvider clientId={clientId}>
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <NextUIProvider>
+          <main className='light text-foreground bg-background'>
+            <App />
+          </main>
+        </NextUIProvider>
+      </QueryClientProvider>
+    </StrictMode>
+  </GoogleOAuthProvider>,
 )
