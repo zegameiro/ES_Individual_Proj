1801000e11c082e093b180a1c1080c8143bf3ab5
zegameiro <jose.mcgameiro@ua.pt>
2024-10-02 22:29:54 +0100

    Added google oauth provider

---
+++ frontend/src/assets/index.jsx
@@ -0,0 +1,9 @@
+import LogoHorizontal from './logo_horizontal.png';
+import LogoVertical from './logo.png';
+import LogoSymbol from './logo_symbol.png';
+
+export {
+    LogoHorizontal,
+    LogoVertical,
+    LogoSymbol
+}
