f754a7a321848c18d80c342972c25f114ee3bd21
zegameiro <jose.mcgameiro@ua.pt>
2024-10-06 17:13:35 +0100

    EIA-7 Added functions to validate an email and a name

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -1,3 +1,5 @@
+import re
+
 def get_postgres_info():
     return {
         "host": "localhost",
@@ -6,3 +8,18 @@ def get_postgres_info():
         "password": "taskflow_admin_password",
         "database": "taskflow_db"
     }
+
+def check_email(email: str) -> bool:
+    email_regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    return bool(re.match(email_regex, email))
+
+def check_name(name: str) -> bool:
+
+    if len(name) < 3 or len(name) > 50:
+        return False
+
+    # Check if any number exists in the name
+    if any(char.isdigit() for char in name):
+        return False
+    
+    return True
