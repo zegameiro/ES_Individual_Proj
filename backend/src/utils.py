d7cc216239dcbe50936b0d11804a85bb283c5a2b
zegameiro <jose.mcgameiro@ua.pt>
2024-10-06 01:38:24 +0100

    EIP-7 Added utils database configurations values

---
+++ backend/src/utils.py
@@ -0,0 +1,8 @@
+def get_postgres_info():
+    return {
+        "host": "localhost",
+        "port": "5432",
+        "user": "taskflow_admin_user",
+        "password": "taskflow_admin_password",
+        "database": "taskflow_db"
+    }
