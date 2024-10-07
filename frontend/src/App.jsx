bf329c0c51a89482d5808769b658268e3e874589
zegameiro <jose.mcgameiro@ua.pt>
2024-10-08 00:24:19 +0100

    EIP-7 Added route to the login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-          </Routes>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+        </Routes>
       </BrowserRouter>
     </>
   )
