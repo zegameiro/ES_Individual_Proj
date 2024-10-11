1d3ab2dc4816bd0fe30e2068f338d5c443a8beb6
zegameiro <jose.mcgameiro@ua.pt>
2024-10-11 17:51:21 +0100

    EIP-7 Added react-cookies and integrated CookieProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { NextUIProvider } from '@nextui-org/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { CookiesProvider } from 'react-cookie'
+
 import App from './App.jsx'
 import './index.css'
 import { clientId } from './constants.jsx'
@@ -12,13 +14,15 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId={clientId} >
     <StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <NextUIProvider>
-          <main className='light text-foreground bg-background'>
-            <App />
-          </main>
-        </NextUIProvider>
-      </QueryClientProvider>
+      <CookiesProvider>
+        <QueryClientProvider client={queryClient}>
+          <NextUIProvider>
+            <main className='light text-foreground bg-background'>
+              <App />
+            </main>
+          </NextUIProvider>
+        </QueryClientProvider>
+      </CookiesProvider>
     </StrictMode>
   </GoogleOAuthProvider>,
 )
