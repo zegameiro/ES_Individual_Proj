4ca5cbfb504380a1122987890122dad9c4b6c895
zegameiro <jose.mcgameiro@ua.pt>
2024-10-19 00:04:36 +0100

    EIP-9 Added task repository

--- backend/src/repositories/task_repositories.py
+++ backend/src/repositories/task_repositories.py
@@ -3,14 +3,19 @@ from sqlalchemy.orm import Session
 from ..models import Task
 from ..schemas import TaskCreate
 
-def create_task(task: TaskCreate, db:Session) -> Task:
+def create_task(task: TaskCreate, user_id: int, db:Session) -> Task:
     
     new_task = Task(
         title=task.title,
         description=task.description,
-        category=task.category,
-        deadline=task.deadline,
-        priority=task.priority
+        user_id = user_id,
     )
 
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+
     return new_task
+
+def get_tasks_from_user(user_id: int, db: Session) -> list[Task]:
+    return db.query(Task).filter(Task.user_id == user_id).all()
