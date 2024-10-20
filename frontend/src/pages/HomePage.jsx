d7eae2febcba8297aba96272a31cd48fa03f529e
zegameiro <jose.mcgameiro@ua.pt>
2024-10-21 00:51:40 +0100

    EIP-9 Added initial version of task page, created landing page and delete stuff from home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,9 @@
 
-import NavBarComp from "../components/NavBarComp"
-
 const HomePage = () => {
 
     return (
-        <div className="flex flex-col">
-            <NavBarComp />
-            <div className="px-[10vw] py-[4vh]">
-                <h1 className="text-2xl font-bold pb-10">Homepage</h1>
-            </div>
+        <div>
+            <h1 className="text-2xl font-bold pb-10">Homepage</h1>
         </div>
     )
 }
