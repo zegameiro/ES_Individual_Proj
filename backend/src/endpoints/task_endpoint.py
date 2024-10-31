264b24e7bbade6b7ebe38892b7e1ca22034e70ec
zegameiro <jose.mcgameiro@ua.pt>
2024-10-31 23:59:08 +0000

    EIP-10 Added endpoint to update a task

--- backend/src/endpoints/task_endpoint.py
+++ backend/src/endpoints/task_endpoint.py
@@ -3,14 +3,14 @@ from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
 from ..database import get_db
-from ..repositories.task_repository import create_task, get_tasks_from_user
+from ..repositories.task_repository import create_task, get_tasks_from_user, update_task
 from ..schemas import TaskCreate, TaskSchema
 from ..utils import authenticated, validate_credential
 
 router = APIRouter()
 
 @router.post(
-    '/', 
+    '', 
     description="Add a new task and associate it with a user. Its required to send the access token in the request",
     name="Add a new Task"
 )
@@ -24,7 +24,7 @@ def add_new_task(request: Request, task: TaskCreate, db_session: Session = Depen
     idinfo = validate_credential(credential)
 
     # Create a new task associated with the user
-    create_task(task=task, user_email=idinfo.get("email"), db=db_session)
+    create_task(task=task, user_email=idinfo.get("email"), db_session=db_session)
 
     return JSONResponse(
         status_code=201,
@@ -34,7 +34,7 @@ def add_new_task(request: Request, task: TaskCreate, db_session: Session = Depen
     )
 
 @router.get(
-    '/',
+    '',
     description="Get tasks associated with a user. Its required to be authenticated",
     name="Get tasks from user"
 )
@@ -48,6 +48,32 @@ def get_tasks(request: Request, db_session: Session = Depends(get_db)):
     idinfo = validate_credential(credential)
 
     # Get tasks associated with the user
-    tasks = get_tasks_from_user(user_email=idinfo.get("email"), db=db_session)
+    tasks = get_tasks_from_user(user_email=idinfo.get("email"), db_session=db_session)
 
     return tasks
+
+@router.put(
+    "",
+    description="Update a specific task that a user created. Its required to be authenticated",
+    name="Update a Task"
+)
+@authenticated()
+def update_created_task(request: Request, task: TaskSchema, db_session: Session = Depends(get_db)):
+
+    print(task)
+
+    # Get the access token from the cookie in the request
+    credential = request.cookies.get('credential')
+
+    # Validate the access token
+    idinfo = validate_credential(credential)
+
+    # Update the task
+    update_task(task=task, task_id=task.id, db_session=db_session)
+
+    return JSONResponse(
+        status_code=200,
+        content={
+            "message": "Task updated successfully",
+        }
+    )
