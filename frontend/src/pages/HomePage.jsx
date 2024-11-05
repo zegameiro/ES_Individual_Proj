5329e322c997fc0c2b63a053c6ceb8af97bceb47
José Gameiro <93797961+zegameiro@users.noreply.github.com>
2024-11-05 23:51:44 +0000

    EIP-14 Added task filtering (#14)

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
-import { useDisclosure, Button, Spinner } from "@nextui-org/react"
-import { useState } from "react"
+import { useDisclosure, Button, Spinner, Select, SelectItem } from "@nextui-org/react"
+import { useState, useMemo } from "react"
 import { useQuery } from "@tanstack/react-query";
 import { PiGraph } from "react-icons/pi";
 
@@ -13,11 +13,24 @@ const HomePage = () => {
 	const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure()
 	const [currentTask, setCurrentTask] = useState()
 	const [isEdit, setIsEdit] = useState(false)
+	const [filters, setFilters] = useState("")
 
 	const { data: tasks, isLoading: loadingTasks } = useQuery({
 		queryKey: ["getTasks"],
 		queryFn: () => getTasks()
 	})
+
+	const filteredTasks = useMemo(() => {
+		if (!tasks || !tasks.data) return [];
+		return tasks.data.filter(task => {
+			if ([...filters]?.includes("completed")) return task.is_completed;
+			if ([...filters]?.includes("incomplete")) return !task.is_completed;
+			if ([...filters]?.includes("low")) return task.priority == "low";
+			if ([...filters]?.includes("medium")) return task.priority == "medium";
+            if ([...filters]?.includes("high")) return task.priority == "high";
+			return true; // no filters applied
+		});
+	}, [filters, tasks]);
 
 	return (
 		<div className="flex flex-col">
@@ -28,11 +41,27 @@ const HomePage = () => {
 			<Legend />
 			<div className="flex flex-col py-5">
 				<AddTaskModal isOpen={isOpen} onOpenChange={onOpenChange} onClose={onClose} currentTask={currentTask} isEdit={isEdit} setIsEdit={setIsEdit} setCurrentTask={setCurrentTask} />
+				<div className="flex flex-row justify-end">
+					<Select 
+						labelPlacement="outside" 
+						className="pb-5 max-w-xs" 
+						variant="underlined" 
+						label="Filter Tasks" 
+						selectedKeys={filters} 
+						onSelectionChange={setFilters}
+					>
+						<SelectItem key="completed">Completed Tasks</SelectItem>
+						<SelectItem key="incomplete">Incomplete Tasks</SelectItem>
+						<SelectItem key="low">Priority Low</SelectItem>
+						<SelectItem key="medium">Priority Medium</SelectItem>
+						<SelectItem key="high">Priority High</SelectItem>
+					</Select>
+				</div>
 				<div className="flex flex-col space-y-4">
 
-					{tasks && tasks?.data?.length > 0 ? ( // Show the list of tasks organized by cards
+					{filteredTasks && filteredTasks?.length > 0 ? ( // Show the list of tasks organized by cards
 						<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-							{tasks.data.map((task, index) => (
+							{filteredTasks?.map((task, index) => (
 								<TaskCard key={index} task={task} onOpen={onOpen} setCurrentTask={setCurrentTask} setIsEdit={setIsEdit} />
 							))}
 						</div>
