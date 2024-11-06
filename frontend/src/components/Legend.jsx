f9c40c666698b32269ee57d82cdc4ebeb7820303
zegameiro <jose.mcgameiro@ua.pt>
2024-11-06 22:49:27 +0000

    EIP-13 Sort tasks is now possible

--- frontend/src/components/Legend.jsx
+++ frontend/src/components/Legend.jsx
@@ -3,7 +3,7 @@ import { FaSquare } from "react-icons/fa";
 
 const Legend = () => {
     return (
-        <div className="flex flex-row space-x-5 w-full py-4">
+        <div className="flex flex-row space-x-5 py-4">
             <span className="flex flex-row items-center gap-2"><FaSquare className="text-blue-500" /> Low Priority</span>
             <span className="flex flex-row items-center gap-2"><FaSquare className="text-warning" /> Medium Priority</span>
             <span className="flex flex-row items-center gap-2"><FaSquare className="text-danger" /> High Priority</span>
