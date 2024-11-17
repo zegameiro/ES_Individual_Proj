ce32e91e5cccb65bd7b37980fa1efeddf78e1526
zegameiro <jose.mcgameiro@ua.pt>
2024-11-17 15:24:45 +0000

    Ready for deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-import { resolve } from 'path'
-
-dotenv.config({ path: resolve(__dirname, '../.env') })
 
 // https://vitejs.dev/config/
 export default defineConfig({
